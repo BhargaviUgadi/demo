d8e6545ccf5d4c6cd5cb003604178749b95049dd
UgadiBhargavi <ugadi.bhargavi@jukshio.com>
2022-01-31 12:53:46 +0530

    first commit

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.myapplication.Hello;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Hello.send(MainActivity.this,"Hello");
     }
 }
--- myapplication/src/main/java/com/example/myapplication/Hello.java
+++ myapplication/src/main/java/com/example/myapplication/Hello.java
@@ -1,11 +1,44 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class Hello {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-    public static void send(Context context,String message){
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+public class Hello extends AppCompatActivity {
+
+    EditText editText,editText1;
+    Button button;
+
+    int x, y;
+    int z;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.hello);
+
+
+        editText=findViewById(R.id.edit);
+        editText1=findViewById(R.id.edit1);
+        button=findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                x=Integer.parseInt(editText.getText().toString());
+                y=Integer.parseInt(editText1.getText().toString());
+                z=x+y;
+                Toast.makeText(Hello.this, z, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
     }
 }
